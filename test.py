9d612fece70973f15b067cf9c8bd6fd4f8c9f9b6
ilyich <ilyich@ngs.ru>
2021-09-06 22:07:08 +0300

    подсчет страниц реализован

--- test.py
+++ test.py
@@ -2,9 +2,8 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import os
 import sys
-from pyPdf import PdfFileReader
+import PyPDF2
 
-PDF_EXTENSION = '.pdf'
 
 class App(tk.Tk):
     def __init__(self):
@@ -21,38 +20,34 @@ class App(tk.Tk):
         btn_exit.pack(padx=60, pady=10)
 
     def choose_file(self):
-        filetypes = (("Текстовый файл", "*.txt"),
+        filetypes = (("Pdf файл", "*.pdf"),
                      ("Изображение", "*.jpg *.gif *.png"),
                      ("Любой", "*"))
         filename = fd.askopenfilename(title="Открыть файл", initialdir="/",
                                       filetypes=filetypes)
         if filename:
             print(filename)
+            pl = open(filename, 'rb')
+            plread = PyPDF2.PdfFileReader(pl)
+            print(plread.numPages)
+
+
 
     def choose_directory(self):
         directory = fd.askdirectory(title="Открыть папку", initialdir="/")
         if directory:
             print(directory)
+            
     
     def onExit(self):
         global app
         app.quit()
     
     def result1(self):
-         total_pages_count = 0
-        for root, dir, files in  os.walk(directory):
-            for file_name in files:
-                if file_name[-len(PDF_EXTENSION):] == PDF_EXTENSION:
-                    file_path = os.path.join(root, file_name)
-                    file_pages_count = pages_count(file_path)
-                    print file_path, file_pages_count
-                total_pages_count += file_pages_count
-        print 'total:', total_pages_count
- 
-    def pages_count(path):
-        return PdfFileReader(file(path, "rb")).getNumPages()
-        a=PdfFileReader(file(path, "rb", FloatingPointError)
-           
+        pl = open('c:/1.pdf', 'rb')
+        plread = PyPDF2.PdfFileReader(pl)
+        print(plread.numPages)
+    
 
 if __name__ == "__main__":
     app = App()
