121bc99b82cb98a2ef4a6e39dae735e3910d3db6
ilyich-ilyich <ilyich@ngs.ru>
2021-09-12 11:38:42 +0300

    сборка перед тестом

--- prod.py
+++ prod.py
@@ -6,6 +6,9 @@ import sys
 import fitz
 import pytesseract
 from PIL import Image
+from openpyxl import load_workbook
+import pandas as pd
+import requests
 
 
 class App(tk.Tk):
@@ -18,61 +21,61 @@ class App(tk.Tk):
         btn_exit.pack(padx=60, pady=10)
 
     def choose_file(self):
-        filetypes = (("Pdf файл", "*.pdf"),
-                     ("Изображение", "*.jpg *.gif *.png"),
+        filetypes = (("Exel файл", "*.xls"),
+                     ("PDF файл", "*.pdf"),
                      ("Любой", "*"))
         filename = fd.askopenfilename(title="Открыть файл", initialdir="/",
                                       filetypes=filetypes)
+        # получили имя файла с путем                              
         if filename:
             print(filename)
-            # pl = open(filename, 'rb')
-            #plread = PyPDF2.PdfFileReader(pl)
-            #print(plread.numPages)
-            pdf_file = fitz.open(filename)
-            #location = input("Enter the location to save: ")
-            location = 'c:\img' 
-            #поиск количества страниц в pdf
-            number_of_pages = len(pdf_file)
-            print('Количество страниц=',number_of_pages)
-            #итерация по каждой странице в pdf
-            for current_page_index in range(number_of_pages):
-            # итерация по каждому изображению на каждой странице PDF
-                for img_index,img in enumerate(pdf_file.getPageImageList(current_page_index)):
-                    xref = img[0]
-                    image = fitz.Pixmap(pdf_file, xref)
-                    # если это чёрно-белое или цветное изображение
-                    img_filename = "{}/image{}-{}.png".format(location,current_page_index, img_index)   
-                    if image.n < 5:
-                             
-                        image.writePNG(img_filename)
-                        #если это CMYK: конвертируем в RGB
-                    else:                
-                        new_image = fitz.Pixmap(fitz.csRGB, image)
-                        new_image.writePNG(img_filename)
-                    img = Image.open(img_filename)
-                    file_name = img.filename
-                    file_name = file_name.split(".")[0]
-                    text = pytesseract.image_to_string(img, lang='rus').strip()
-                    print(text)
-                    with open(f'{file_name}.txt', 'w') as text_file:
-                        text_file.write(text)    
+            # работа с экселем
+            wb = load_workbook(filename) # файл с ссылками на сканы актов возврата БУ
+            ws = wb.active # выбор активной страницы
+            headers = {
+                        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15",
+                        "Accept-Language": "en"
+                    }
+            for i in range(3, ws.max_row + 1):  # перебираем строки
+                nom_bu = (ws.cell(row=i, column=4).value)  # получаем номер БУ 
+                adressURL=(ws.cell(row=i, column=9).value) # получаем ссылку
+                send=requests.get(adressURL, headers=headers) # загружаем файл в объект
+                name_of_PDF_file ='c:/bu/tmp/' +str(ws.cell(row=6, column=4).value)+'.pdf'
+                print(name_of_PDF_file)
+                if send.status_code == 200:  # если успешно  записывваем объект как файл pdf
+                print('Success!')
+                with open(name_of_PDF_file,"wb")as code:
+                    code.write(send.content) #записываем результат  в папку tmp
+                    
+                # работа с pdf
+                pdf_file = fitz.open(name_of_PDF_file) #  открываем скачаный файл
+                location = 'c:/bu/tmp/'  #  папка для текстовых файлов
+                number_of_pages = len(pdf_file) #подсчет количества страниц в pdf
+                print('Количество страниц=',number_of_pages)
+                for current_page_index in range(number_of_pages): #итерация по каждой странице в pdf
+                    for img_index,img in enumerate(pdf_file.getPageImageList(current_page_index)): # итерация по каждому изображению на каждой странице PDF
+                        xref = img[0]
+                        image = fitz.Pixmap(pdf_file, xref)
+                        # если это чёрно-белое или цветное изображение
+                        img_filename = "{}/image{}-{}.png".format(location,current_page_index, img_index)   
+                        if image.n < 5:
+                                 
+                            image.writePNG(img_filename)
+                            #если это CMYK: конвертируем в RGB
+                        else:                
+                            new_image = fitz.Pixmap(fitz.csRGB, image)
+                            new_image.writePNG(img_filename)
+                        img = Image.open(img_filename)
+                        file_name = img.filename
+                        file_name = file_name.split(".")[0]
+                        text = pytesseract.image_to_string(img, lang='rus').strip()
+                        print(text)
+                        with open(f'{file_name}.txt', 'w') as text_file:
+                            text_file.write(text)    
 
-
-
-    def choose_directory(self):
-        directory = fd.askdirectory(title="Открыть папку", initialdir="/")
-        if directory:
-            print(directory)
-            
-    
     def onExit(self):
         global app
         app.quit()
-    
-    def result1(self):
-        pl = open('c:/1.pdf', 'rb')
-        plread = PyPDF2.PdfFileReader(pl)
-        print(plread.numPages)
     
 
 if __name__ == "__main__":
